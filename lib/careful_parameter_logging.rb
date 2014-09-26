c4e9058b047b96295f4f7673cb396a8c3b31ce96
Blake Hitchcock <rbhitchcock@gmail.com>
2014-09-25 20:57:20 -0400

    Oops don't require nonexistent stuff

--- lib/careful_parameter_logging.rb
+++ lib/careful_parameter_logging.rb
@@ -2,5 +2,4 @@ if defined? Rails
   require 'careful_parameter_logging/middleware'
   require 'careful_parameter_logging/controller'
   require 'careful_parameter_logging/railtie'
-  require 'careful_parameter_logging/filter_parameters'
 end
